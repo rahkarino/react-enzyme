35cae3fe1e50a5412e133782f2ce0bd279b2cacd
Ehsan Safari <ehsansafari@hotmail.com>
2021-09-08 10:55:36 +0430

    redux get posts with tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import Header from "./components/header";
 import "./app.scss";
 import HeadLine from "./components/headline";
+import Button from "./components/button";
+import ListItem from "./components/listItem";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <section className="main">
-        <HeadLine header="Posts" desc="click the button to render posts" />
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetch = this.fetch.bind(this);
+  }
+  fetch() {
+    this.props.fetchPosts();
+  }
+  render() {
+    const configButton = {
+      buttonText: "Get Posts",
+      emitEvent: this.fetch,
+    };
+    const { posts } = this.props;
+    return (
+      <div className="App">
+        <Header />
+        <section className="main">
+          <HeadLine header="Posts" desc="click the button to render posts" />
+          <Button {...configButton} />
+          {posts.length > 0 && (
+            <div>
+              {posts.map((post, index) => {
+                const { title, body } = post;
+                const configList = {
+                  title,
+                  desc: body,
+                };
+                return <ListItem key={index} {...configList} />
+              })}
+            </div>
+          )}
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(App);
--- src/components/listItem/index.js
+++ src/components/listItem/index.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class ListItem extends Component {
-    render() {
-        return (
-            <div data-test="listItemComponent">test</div>
-        )
+  render() {
+    const { title, desc } = this.props;
+    if (!title) {
+      return null;
     }
+    return (
+      <div className="container">
+        <div data-test="listItemComponent">
+          <h3 data-test="componentTitle">{title}</h3>
+          <p data-test="componentDesc">{desc}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 ListItem.propTypes = {
-    title: PropTypes.string,
-    desc: PropTypes.string
+  title: PropTypes.string,
+  desc: PropTypes.string,
 };
 
-export default ListItem
+export default ListItem;
